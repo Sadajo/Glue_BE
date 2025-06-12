156a6f2955f9716e8deca1c7b722acbde5da1228
kim-minkyoung <decomin02@pusan.ac.kr>
2025-06-13 01:26:38 +0900

    feat(Group): @JsonInclude(JsonInclude.Include.NON_NULL) 추가

--- src/main/java/org/glue/glue_be/chat/dto/response/GroupChatRoomLeaveResult.java
+++ src/main/java/org/glue/glue_be/chat/dto/response/GroupChatRoomLeaveResult.java
@@ -1,7 +1,10 @@
 package org.glue.glue_be.chat.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.util.List;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record GroupChatRoomLeaveResult(
         List<ActionResponse> status,
         ActionResponse leaveMessage
